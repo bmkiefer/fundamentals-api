12f5e3b4d26998a206f43a8c2045a206e308bf6d
Brandon Michael Kiefer <brandonmichaelkiefer@wireless-nat-inside.uiowa.edu>
2014-04-11 22:58:57 -0500

    added content area

--- app/controllers/api/v1/sub_providers_controller.rb
+++ app/controllers/api/v1/sub_providers_controller.rb
@@ -9,14 +9,21 @@ class Api::V1::SubProvidersController < ApplicationController
 
   def index
         subscriptions = Subscription.where(:provider_id => params[:id])
+	
+	provider = Provider.find(params[:id])
+
+	content_area = ContentArea.find(provider.content_area_id)
+
+	delivery_mode = DeliveryMode.find(provider.delivery_mode_id)
 
 	render :status => 200,
            :json => { :success => true,
                       :info => "List of Subscriptions",
                       :data => {
 
-                                   "subscriptions" => subscriptions
-                                   
+                                   "subscriptions" => subscriptions ,
+                                   "content_area" => content_area.name,
+			           "delivery_mode" => delivery_mode.name
                                }
                     }
   end
