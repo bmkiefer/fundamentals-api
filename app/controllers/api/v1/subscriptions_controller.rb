037cd4608321cff4a2d3edbe64c92d1579f0d85b
Brandon Michael Kiefer <brandonmichaelkiefer@wireless-nat-inside.uiowa.edu>
2014-04-27 20:49:39 -0500

    changed function name

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -28,7 +28,7 @@ class Api::V1::SubscriptionsController < ApplicationController
                     }
   end
 
-  def destroy
+  def delete
 	Subscription.find(params[:sub_id]).delete
 
         render :status => 200,
