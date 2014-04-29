6cae502c8292da28e78d872b9fdf1220546ce9be
Brandon Michael Kiefer <brandonmichaelkiefer@wireless-nat-inside.uiowa.edu>
2014-04-29 16:19:31 -0500

    added routes

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -11,7 +11,8 @@ class Api::V1::SessionsController < Devise::SessionsController
     render :status => 200,
            :json => { :success => true,
                       :info => "Logged in",
-                      :data => { :auth_token => current_user.authentication_token,
+                      :data => { :user => current_user
+				 :auth_token => current_user.authentication_token,
 				 :admin_flag => current_user.admin_flag,
 				 :content_manager_flag => current_user.content_manager_flag
 				 } }
